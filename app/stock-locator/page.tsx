9fbe7c955d20a935a10aa7397fa75cb77a11859a
Natip85 <106977567+Natip85@users.noreply.github.com>
2024-08-14 16:17:57 +0300

    starting stock locator page

--- app/stock-locator/page.tsx
+++ app/stock-locator/page.tsx
@@ -1,3 +1,117 @@
+"use client";
+import { Tabs } from "@/components/Tabs";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function StockLocatorPage() {
-  return <div>StockLocatorPage</div>;
+  const tabs = [
+    {
+      title: "Products",
+      value: "products",
+      content: (
+        <div className="relative h-full w-full overflow-hidden rounded-2xl bg-gradient-to-br from-[#18844e] to-[#1AB266] p-4 text-xl font-bold md:text-4xl">
+          <p>Products</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "Pharmacies",
+      value: "pharmacies",
+      content: (
+        <div className="relative h-full w-full overflow-hidden rounded-2xl bg-gradient-to-br from-[#18844e] to-[#1AB266] p-4 text-xl font-bold md:text-4xl">
+          <p>Pharmacies</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className="relative mx-auto my-40 flex h-screen w-full max-w-7xl flex-col items-start justify-start p-4 [perspective:1000px]">
+      <Tabs tabs={tabs} />
+    </div>
+  );
 }
+const DummyContent = () => {
+  const invoices = [
+    {
+      invoice: "INV001",
+      paymentStatus: "Paid",
+      totalAmount: "$250.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV002",
+      paymentStatus: "Pending",
+      totalAmount: "$150.00",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV003",
+      paymentStatus: "Unpaid",
+      totalAmount: "$350.00",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV004",
+      paymentStatus: "Paid",
+      totalAmount: "$450.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV005",
+      paymentStatus: "Paid",
+      totalAmount: "$550.00",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV006",
+      paymentStatus: "Pending",
+      totalAmount: "$200.00",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV007",
+      paymentStatus: "Unpaid",
+      totalAmount: "$300.00",
+      paymentMethod: "Credit Card",
+    },
+  ];
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+};
